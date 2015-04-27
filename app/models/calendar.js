ff5d4b6ccbb204cdb04ddb6be6872f9cc62598d4
bradyJ89 <omfguess@aim.com>
2015-04-27 00:00:28 -0400

    update event schema

--- app/models/calendar.js
+++ app/models/calendar.js
@@ -4,6 +4,9 @@ var mongoose = require('mongoose');
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Calendar', {
-    name : {type : String, default: ''}
+var Schema = mongoose.Schema;
+var eventSchema = new Schema({
+	trainer : Number,
+	message : String
 });
+module.exports = mongoose.model('Event', eventSchema);
