9af384ae15e0d29ab1e01f5ab51b69659be7e50b
bradyJ89 <omfguess@aim.com>
2015-05-04 14:24:55 -0400

    capture and display local video stream

--- public/js/controllers/VideoChatCtrl.js
+++ public/js/controllers/VideoChatCtrl.js
@@ -2,4 +2,34 @@ angular.module("VideoChatCtrl", ["ui.bootstrap"]).controller("VideoChatCtrl", fu
 
 	//document.write("The Chat Controller");
 	console.log("we made it to the chat view");
+	var localStream, localPeerConnection, remotePeerConnection;
+
+
+	function trace(text) {
+	  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+	}
+
+	function gotStream(stream){
+	  trace("Received local stream");
+	  var localVideo = $('#localVideo')[0];
+	  localVideo.src = URL.createObjectURL(stream);
+	  localStream = stream;
+	  callButton.disabled = false;
+	}
+
+	$scope.start = function() {
+	  trace("Requesting local stream");
+	  startButton.disabled = true;
+	  getUserMedia({audio:true, video:true}, gotStream,
+	    function(error) {
+	      trace("getUserMedia error: ", error);
+	    });
+	}
+	
+
+	/*$scope.hangUp = function(){
+		console.log("stopping the stream");
+		window.stream.stop();
+		$('#localStream').attr('src', "");
+	}*/
 });
