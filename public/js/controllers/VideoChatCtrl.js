45f8c6c2ec72166b30b34923e296b17124c1ee99
bradyJ89 <omfguess@aim.com>
2015-05-04 00:42:36 -0400

    add header to chat view

--- public/js/controllers/VideoChatCtrl.js
+++ public/js/controllers/VideoChatCtrl.js
@@ -1,5 +1,5 @@
 angular.module("VideoChatCtrl", ["ui.bootstrap"]).controller("VideoChatCtrl", function($scope){
 
-	document.write("The Chat Controller");
-
+	//document.write("The Chat Controller");
+	console.log("we made it to the chat view");
 });
