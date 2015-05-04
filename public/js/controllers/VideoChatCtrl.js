c47a3fca65d785faac47582e74b1626f05e34f87
bradyJ89 <omfguess@aim.com>
2015-05-04 15:48:59 -0400

    connect/disconnect when chat view is visible

--- public/js/controllers/VideoChatCtrl.js
+++ public/js/controllers/VideoChatCtrl.js
@@ -2,6 +2,19 @@ angular.module("VideoChatCtrl", ["ui.bootstrap"]).controller("VideoChatCtrl", fu
 
 	//document.write("The Chat Controller");
 	console.log("we made it to the chat view");
+
+	var socket = io.connect();
+    socket.connect('http://127.0.0.1:1337');
+
+    socket.on("connect", function(socket){
+    	console.log("new socket: " + socket);
+    });
+
+    $scope.$on("$destroy", function(){
+    	socket.disconnect();
+    	socket.emit("disconnect");
+    });
+
 	var localStream, localPeerConnection, remotePeerConnection;
 
 
@@ -14,7 +27,7 @@ angular.module("VideoChatCtrl", ["ui.bootstrap"]).controller("VideoChatCtrl", fu
 	  var localVideo = $('#localVideo')[0];
 	  localVideo.src = URL.createObjectURL(stream);
 	  localStream = stream;
-	  callButton.disabled = false;
+	 //callButton.disabled = false;
 	}
 
 	$scope.start = function() {
@@ -24,12 +37,90 @@ angular.module("VideoChatCtrl", ["ui.bootstrap"]).controller("VideoChatCtrl", fu
 	    function(error) {
 	      trace("getUserMedia error: ", error);
 	    });
-	}
-	
 
-	/*$scope.hangUp = function(){
-		console.log("stopping the stream");
-		window.stream.stop();
-		$('#localStream').attr('src', "");
-	}*/
+	  socket.on("connect", function(){
+	  	console.log("user connected");
+	  });
+	}
+
+	$scope.endCall = function(){
+		localStream.stop();
+		$('#localVideo').attr('src', "");
+	}
+
+	$scope.call = function() {
+	  //callButton.disabled = true;
+	  //hangupButton.disabled = false;
+	  console.log("calling");
+	  trace("Starting call");
+
+	  if (localStream.getVideoTracks().length > 0) {
+	    trace('Using video device: ' + localStream.getVideoTracks()[0].label);
+	  }
+	  if (localStream.getAudioTracks().length > 0) {
+	    trace('Using audio device: ' + localStream.getAudioTracks()[0].label);
+	  }
+
+	  var servers = null;
+
+	  localPeerConnection = new RTCPeerConnection(servers);
+	  trace("Created local peer connection object localPeerConnection");
+	  localPeerConnection.onicecandidate = gotLocalIceCandidate;
+
+	  remotePeerConnection = new RTCPeerConnection(servers);
+	  trace("Created remote peer connection object remotePeerConnection");
+	  remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
+	  remotePeerConnection.onaddstream = gotRemoteStream;
+
+	  localPeerConnection.addStream(localStream);
+	  trace("Added localStream to localPeerConnection");
+	  localPeerConnection.createOffer(gotLocalDescription,handleError);
+	}
+
+	function gotLocalDescription(description){
+	  localPeerConnection.setLocalDescription(description);
+	  trace("Offer from localPeerConnection: \n" + description.sdp);
+	  remotePeerConnection.setRemoteDescription(description);
+	  remotePeerConnection.createAnswer(gotRemoteDescription,handleError);
+	}
+
+	function gotRemoteDescription(description){
+	  remotePeerConnection.setLocalDescription(description);
+	  trace("Answer from remotePeerConnection: \n" + description.sdp);
+	  localPeerConnection.setRemoteDescription(description);
+	}
+
+	$scope.hangup = function hangup() {
+	  trace("Ending call");
+	  localPeerConnection.close();
+	  remotePeerConnection.close();
+	  localPeerConnection = null;
+	  remotePeerConnection = null;
+	  //hangupButton.disabled = true;
+	  //callButton.disabled = false;
+	}
+
+	function gotRemoteStream(event){
+	  remoteVideo.src = URL.createObjectURL(event.stream);
+	  trace("Received remote stream");
+	  console.log(event);
+	}
+
+	function gotLocalIceCandidate(event){
+	  console.log(event);
+	  if (event.candidate) {
+	    remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+	    trace("Local ICE candidate: \n" + event.candidate.candidate);
+	  }
+	}
+
+	function gotRemoteIceCandidate(event){
+	  if (event.candidate) {
+	    localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
+	    trace("Remote ICE candidate: \n " + event.candidate.candidate);
+	  }
+	}
+
+	function handleError(){}
+
 });
